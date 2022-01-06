49092d94bcff51c2446da2800b4b73632f03e2b4
RawFisher <656266431@qq.com>
2022-01-06 15:57:43 +0800

    PAFPN add an output exp

---
+++ exps/example/custom/jiaodiao_enhance_yolox_tiny.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+import os
+
+from yolox.exp.enhance_yolox import EnhanceExp as MyExp
+
+
+class Exp(MyExp):
+    def __init__(self):
+        super(Exp, self).__init__()
+        self.depth = 0.33
+        self.width = 0.375
+        self.input_scale = (416, 416)
+        self.mosaic_scale = (0.5, 1.5)
+        self.random_size = (10, 20)
+        self.test_size = (416, 416)
+        self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
+        self.enable_mixup = False
+
+        # Define yourself dataset path
+        self.data_dir = "datasets/jiaodiao"
+        self.train_ann = "instances_train2017.json"
+        self.val_ann = "instances_val2017.json"
+
+        self.num_classes = 9
+
+        self.max_epoch = 200
+        self.data_num_workers = 4
+        self.eval_interval = 1
+
+        self.basic_lr_per_img = 0.02 / 64.0  # 0.01 / 64.0
+        self.min_lr_ratio = 0.1  # 0.05
